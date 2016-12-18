8d7eec905426ebb0fc728a3a71372631ef435928
zhangchi <151250190@smail.nju.edu.cn>
2016-12-18 19:32:23 +0800

    a

---
+++ Orhssystem/src/main/java/com/edu/nju/acm/Orhssystem/App.java
@@ -0,0 +1,14 @@
+package com.edu.nju.acm.Orhssystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      Main a = new Main();
+      a.calcu();
+    }
+}
---
+++ Orhssystem/src/main/java/com/edu/nju/acm/Orhssystem/Main.java
@@ -0,0 +1,11 @@
+package com.edu.nju.acm.Orhssystem;
+
+public class Main {
+	void calcu(){
+		int a=0;
+		for(int i=0;i<100000;i++){
+			a++;
+			}
+		}
+		
+}
